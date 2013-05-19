463aa4d29616678c0beb3a9e88790e75d2145e77
Yasushi Hisatomi <hisatomiy@gmail.com>
2013-05-19 16:37:25 +0900

    2回目

---
+++ src/kihon/Sample.java
@@ -0,0 +1,16 @@
+package kihon;
+
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x=2, y=3;
+		System.out.println(x+y);
+
+
+	}
+
+}
