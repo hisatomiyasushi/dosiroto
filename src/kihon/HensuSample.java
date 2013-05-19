244f8af60a52032fe4fe454f68cfd36611e8558f
Yasushi Hisatomi <hisatomiy@gmail.com>
2013-05-19 16:00:41 +0900

    初めてのコミット

---
+++ src/kihon/HensuSample.java
@@ -0,0 +1,25 @@
+package kihon;
+
+public class HensuSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ]
+
+		int x;
+		double y;
+		String z;
+
+		x=1;
+		y=1.5;
+		z="Hello";
+
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+
+	}
+
+}
